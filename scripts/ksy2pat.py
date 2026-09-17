c50aa0ddd32527eaa998774ee09a32e66de683f6
Dāvis Mosāns <davispuh@gmail.com>
2026-09-18 02:08:15 +0300

    scripts/ksy2pat: implement support for `type: switch-on / cases`

--- scripts/ksy2pat.py
+++ scripts/ksy2pat.py
@@ -21,9 +21,9 @@ def fixTypeName(name):
 
     return name
 
-def format_comment(comment):
+def format_comment(comment, indent = " "):
     if comment != "":
-        return " // " + comment.replace('\n', ' ')
+        return indent + "// " + comment.replace('\n', ' ')
     return ""
 
 def declare_variable(name, entry_type, array_size, bitfield_field_size):
@@ -81,6 +81,20 @@ def get_entry_type_size(entry):
 
     return entry_type, array_size
 
+def get_entry_types(entry):
+    entry_types = {}
+    array_size = None
+    if isinstance(entry.get("type"), dict):
+        for expr, type in entry["type"].get("cases", {}).items():
+            type_entry = dict(entry)
+            type_entry["type"] = type
+            expr = handle_expr(expr)
+            entry_types[expr], array_size = get_entry_type_size(type_entry)
+    else:
+        entry_types[None], array_size = get_entry_type_size(entry)
+
+    return entry_types, array_size
+
 def handle_meta_xref(xref):
     if "mime" in xref:
         add_line(f"#pragma MIME {xref['mime']}")
@@ -102,6 +116,9 @@ def handle_meta(meta):
     if "id" in meta:
         global top_level_struct_name
         top_level_struct_name = str(meta["id"]).capitalize()
+
+def handle_expr(expr):
+    return str(expr)
 
 def handle_types(types):
     result = ""
@@ -143,26 +160,47 @@ def handle_seq(seq):
 
     for entry in seq:
         name = entry["id"]
-        bitfield_field_size = ""
         docs = ""
+        new_line = ""
+        indent = "    "
+        indent_count = 1
 
         if "doc" in entry:
             docs = entry["doc"]
 
-        entry_type, array_size = get_entry_type_size(entry)
+        entry_types, real_array_size = get_entry_types(entry)
+        array_size = real_array_size
+        match = None
+        if isinstance(entry.get("type"), dict):
+            match = entry["type"]["switch-on"]
 
-        if re.compile("^b[0-9]+$").match(entry_type):
-            is_bitfield = True
-            bitfield_field_size = int(entry_type[1:])
+        type_index = 0
+        for expr, entry_type in entry_types.items():
+            bitfield_field_size = ""
+            if re.compile("^b[0-9]+$").match(entry_type):
+                is_bitfield = True
+                bitfield_field_size = int(entry_type[1:])
 
-        new_line = ""
+            if "if" in entry and type_index == 0:
+                condition = handle_expr(entry['if'])
+                new_line += indent * indent_count + f"if ({condition})\n"
+                indent_count += 1
 
-        if "if" in entry:
-            new_line += f"    if ({entry['if']})\n    "
-        
-        new_line += "    " + declare_variable(name, entry_type, array_size, bitfield_field_size)
+            if match:
+                if type_index == 0:
+                    if docs != "":
+                        new_line += format_comment(docs, indent * indent_count) + "\n"
+                    new_line += indent * indent_count + f"match ({match}) {{\n"
+                new_line += indent * (indent_count + 1) + f"({expr}): " + declare_variable(name, entry_type, array_size, bitfield_field_size) + "\n"
+            else:
+                new_line += indent * indent_count + declare_variable(name, entry_type, array_size, bitfield_field_size)
 
-        new_line += format_comment(docs)
+            type_index += 1
+
+        if match:
+            new_line += indent * indent_count + "}"
+        else:
+            new_line += format_comment(docs)
 
         lines.append(new_line)
 
